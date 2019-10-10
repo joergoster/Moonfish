85acb8feb11b4c2090ce8d3b85f62eed77df3788
joergoster <osterj165@googlemail.com>
2019-10-10 19:10:44 +0200

    Rename.

--- src/misc.cpp
+++ src/misc.cpp
@@ -127,7 +127,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "Stockfish " << Version << setfill('0');
+  ss << "Moonfish " << Version << setfill('0');
 
   if (Version.empty())
   {
